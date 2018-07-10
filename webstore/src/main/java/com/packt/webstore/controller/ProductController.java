3279d75a60e81c5b253c1acff791066a51e7721f
EL Alami Aymane <elalamiaymane97@gmail.com>
2018-07-10 16:24:22 +0100

    Fixed everything

--- webstore/src/main/java/com/packt/webstore/controller/ProductController.java
+++ webstore/src/main/java/com/packt/webstore/controller/ProductController.java
@@ -1,22 +1,20 @@
 package com.packt.webstore.controller;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.packt.webstore.domain.Product;
-import com.packt.webstore.domain.repository.ProductRepository;
+import com.packt.webstore.service.impl.ProductServiceImpl;
 
 @Controller
 public class ProductController {
 	@Autowired
-	private ProductRepository productRepository;
+	private ProductServiceImpl productServiceImpl;
 
 	@RequestMapping("/products")
 	public String list(Model model) {
-		model.addAttribute("products", productRepository.getAllProducts());
+		model.addAttribute("products", productServiceImpl.getAllProducts());
 		return "products";
 	}
 }
