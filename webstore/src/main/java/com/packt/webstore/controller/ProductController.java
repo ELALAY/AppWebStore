8e4f7747ff6354369c3c21f456c579d2a984e14c
EL Alami Aymane <elalamiaymane97@gmail.com>
2018-07-12 18:01:59 +0100

    First Commit added the dispatcherServelet

--- webstore/src/main/java/com/packt/webstore/controller/ProductController.java
+++ webstore/src/main/java/com/packt/webstore/controller/ProductController.java
@@ -9,9 +9,9 @@ import com.packt.webstore.domain.Product;
 @Controller
 public class ProductController {
 
-  @RequestMapping("/products")
+  @RequestMapping("/welcome")
   public String list(Model model) {
-    Product iphone = new Product("P1234","iPhone 5s", new  BigDecimal(500));
+    Product iphone = new Product("P1234","iPhone 5s", new BigDecimal(500));
     iphone.setDescription("Apple iPhone 5s smartphone with 4.00-inch 640x1136 display and 8-megapixel rear camera");
     iphone.setCategory("Smart Phone");
     iphone.setManufacturer("Apple");
