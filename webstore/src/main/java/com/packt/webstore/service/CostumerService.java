828d2be9c225df14f223f636fb5195671c6d875a
EL Alami Aymane <elalamiaymane97@gmail.com>
2018-07-12 14:35:57 +0100

    First Commit

--- webstore/src/main/java/com/packt/webstore/service/CostumerService.java
+++
@@ -1,9 +0,0 @@
-package com.packt.webstore.service;
-
-import java.util.List;
-
-import com.packt.webstore.domain.Costumer;
-
-public interface CostumerService {
-	List<Costumer> getAllCostumers();
-}
