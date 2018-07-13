513fcc44755986ce1f032c389e961b4c34396c47
EL Alami Aymane <elalamiaymane97@gmail.com>
2018-07-13 16:55:15 +0100

    rest the file

--- webstore/src/main/java/com/packt/webstore/controller/HomeController.java
+++ webstore/src/main/java/com/packt/webstore/controller/HomeController.java
@@ -1,8 +1,12 @@
 package com.packt.webstore.controller;
 
+import java.math.BigDecimal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.packt.webstore.domain.Product;
 
 @Controller
 public class HomeController {
@@ -11,7 +15,9 @@ public class HomeController {
 	public String welcome(Model model) {
 		String str = "welcome";
 		model.addAttribute("greeting", "Welcome to Web Store!");
-		model.addAttribute("tagline", "The one and only amazing webstore");
+		model.addAttribute("tagline", "The one and only amazing webstore");		
+		System.out.println("Here in the homecontroller");
+		
 		return str;
 	}
 	
