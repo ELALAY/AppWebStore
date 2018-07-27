c2f0f3ebc2ffe291f43b561647cbd8776f2c214f
EL Alami Aymane <elalamiaymane97@gmail.com>
2018-07-27 11:52:10 +0100

    trying to work on filebased database for the products

--- webstore/src/main/java/com/packt/webstore/domain/repository/impl/InMemoryProductRepository.java
+++ webstore/src/main/java/com/packt/webstore/domain/repository/impl/InMemoryProductRepository.java
@@ -1,6 +1,9 @@
 package com.packt.webstore.domain.repository.impl;
 
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -13,29 +16,30 @@ import com.packt.webstore.domain.Product;
 import com.packt.webstore.domain.repository.ProductRepository;
 
 @Repository
-public class InMemoryProductRepository implements ProductRepository{
-	
+public class InMemoryProductRepository implements ProductRepository {
+
 	private List<Product> listOfProducts = new ArrayList<Product>();
-	
+
 	public InMemoryProductRepository() {
-		Product iphone = new Product("P1234","iPhone 5s", 500);
+		Product iphone = new Product("P1234", "iPhone 5s", 500);
 		iphone.setDescription("Apple iPhone 5s smartphone with 4.00-inch 640x1136 display and 8-megapixel rear camera");
 		iphone.setCategory("Smart Phone");
 		iphone.setManufacturer("Apple");
 		iphone.setUnitsInStock(1000);
-		
-		Product laptop_dell = new Product("P1235","Dell Inspiron", 700);
+
+		Product laptop_dell = new Product("P1235", "Dell Inspiron", 700);
 		laptop_dell.setDescription("Dell Inspiron 14-inch Laptop (Black) with 3rd Generation Intel Core processors");
 		laptop_dell.setCategory("Laptop");
 		laptop_dell.setManufacturer("Dell");
 		laptop_dell.setUnitsInStock(1000);
-		
-		Product tablet_Nexus = new Product("P1236","Nexus 7", 300);
-		tablet_Nexus.setDescription("Google Nexus 7 is the lightest 7 inch tablet With a quad-core Qualcomm Snapdragon™ S4 Pro processor");
+
+		Product tablet_Nexus = new Product("P1236", "Nexus 7", 300);
+		tablet_Nexus.setDescription(
+				"Google Nexus 7 is the lightest 7 inch tablet With a quad-core Qualcomm Snapdragon™ S4 Pro processor");
 		tablet_Nexus.setCategory("Tablet");
 		tablet_Nexus.setManufacturer("Google");
 		tablet_Nexus.setUnitsInStock(1000);
-		
+
 		listOfProducts.add(iphone);
 		listOfProducts.add(laptop_dell);
 		listOfProducts.add(tablet_Nexus);
@@ -48,30 +52,104 @@ public class InMemoryProductRepository implements ProductRepository{
 
 	public Product getProductById(String productId) {
 		Product productById = null;
-		
-		for(Product product : listOfProducts) {
-			if(product!=null && product.getProductId()!=null && product.getProductId().equals(productId)){
+
+		for (Product product : listOfProducts) {
+			if (product != null && product.getProductId() != null && product.getProductId().equals(productId)) {
 				productById = product;
 				break;
 			}
 		}
-		
-		if(productById == null){
-			throw new IllegalArgumentException("No products found with the product id: "+ productId);
+
+		if (productById == null) {
+			throw new IllegalArgumentException("No products found with the product id: " + productId);
 		}
-		
+
 		return productById;
 	}
-	
+
+	public void ReadProducts() throws IOException {
+		String fileName = "Products,txt";
+		String line = null;
+		Product readProduct = new Product();
+
+		try {
+			// FileReader reads text files in the default encoding.
+			FileReader fileReader = new FileReader(fileName);
+
+			// Always wrap FileReader in BufferedReader.
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+			if ((line = bufferedReader.readLine()) != null) {
+				readProduct.setProductId(line);
+			} else if ((line = bufferedReader.readLine()) != null) {
+				if (!line.equals("/"))
+					readProduct.setName(line);
+			} else if ((line = bufferedReader.readLine()) != null) {
+				if (!line.equals("/"))
+					readProduct.setUnitPrice(Double.parseDouble(line));
+			} else if ((line = bufferedReader.readLine()) != null) {
+				if (!line.equals("/"))
+					readProduct.setDescription(line);
+			} else if ((line = bufferedReader.readLine()) != null) {
+				if (!line.equals("/"))
+					readProduct.setManufacturer(line);
+			} else if ((line = bufferedReader.readLine()) != null) {
+				if (!line.equals("/"))
+					readProduct.setCategory(line);
+			} else if ((line = bufferedReader.readLine()) != null) {
+				if (!line.equals("/"))
+					readProduct.setUnitsInStock(Long.parseLong(line));
+			} else if ((line = bufferedReader.readLine()) != null) {
+				if (!line.equals("/"))
+					readProduct.setUnitsInOrder(Long.parseLong(line));
+			} else if ((line = bufferedReader.readLine()) != null) {
+				if (!line.equals("/"))
+					readProduct.setDiscontinued(Boolean.parseBoolean(line));
+			}
+
+			// Always close files.
+			bufferedReader.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("Unable to open file '" + fileName + "'");
+		} catch (IOException ex) {
+			System.out.println("Error reading file '" + fileName + "'");
+			// Or we could just do this:
+			// ex.printStackTrace();
+		}
+
+	}
+
+	public void WriteProducts() {
+		/*
+		 * if ((line = bufferedReader.readLine()) != null) {
+		 * readProduct.setProductId(line); } else if ((line = bufferedReader.readLine())
+		 * != null) { if (!line.equals("/")) readProduct.setName(line); } else if ((line
+		 * = bufferedReader.readLine()) != null) { if (!line.equals("/"))
+		 * readProduct.setUnitPrice(Double.parseDouble(line)); } else if ((line =
+		 * bufferedReader.readLine()) != null) { if (!line.equals("/"))
+		 * readProduct.setDescription(line); } else if ((line =
+		 * bufferedReader.readLine()) != null) { if (!line.equals("/"))
+		 * readProduct.setManufacturer(line); } else if ((line =
+		 * bufferedReader.readLine()) != null) { if (!line.equals("/"))
+		 * readProduct.setCategory(line); } else if ((line = bufferedReader.readLine())
+		 * != null) { if (!line.equals("/"))
+		 * readProduct.setUnitsInStock(Long.parseLong(line)); } else if ((line =
+		 * bufferedReader.readLine()) != null) { if (!line.equals("/"))
+		 * readProduct.setUnitsInOrder(Long.parseLong(line)); } else if ((line =
+		 * bufferedReader.readLine()) != null) { if (!line.equals("/"))
+		 * readProduct.setDiscontinued(Boolean.parseBoolean(line)); }
+		 */
+	}
+
 	public List<Product> getProductsByCategory(String category) {
 		List<Product> productsByCategory = new ArrayList<Product>();
-			
-		for(Product product: listOfProducts) {
-			if(category.equalsIgnoreCase(product.getCategory())){
+
+		for (Product product : listOfProducts) {
+			if (category.equalsIgnoreCase(product.getCategory())) {
 				productsByCategory.add(product);
 			}
 		}
-		
+
 		return productsByCategory;
 	}
 
@@ -80,28 +158,28 @@ public class InMemoryProductRepository implements ProductRepository{
 		Set<Product> productsByCategory = new HashSet<Product>();
 
 		Set<String> criterias = filterParams.keySet();
-		
-		if(criterias.contains("brand")) {
-			for(String brandName: filterParams.get("brand")) {
-				for(Product product: listOfProducts) {
-					if(brandName.equalsIgnoreCase(product.getManufacturer())){
+
+		if (criterias.contains("brand")) {
+			for (String brandName : filterParams.get("brand")) {
+				for (Product product : listOfProducts) {
+					if (brandName.equalsIgnoreCase(product.getManufacturer())) {
 						productsByBrand.add(product);
 					}
 				}
 			}
 		}
-		
-		if(criterias.contains("category")) {
-			for(String category: filterParams.get("category")) {
+
+		if (criterias.contains("category")) {
+			for (String category : filterParams.get("category")) {
 				productsByCategory.addAll(this.getProductsByCategory(category));
 			}
 		}
-		
+
 		productsByCategory.retainAll(productsByBrand);
-		
+
 		return productsByCategory;
 	}
-	
+
 	public void addProduct(Product product) {
 		listOfProducts.add(product);
 	}
